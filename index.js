a1202761f9c6b8dbb1fe0faf0f590257769e24a0
Joseph Post <jp@jsphpst.com>
2016-01-26 23:52:22 -0600

    simplify script

--- index.js
+++ index.js
@@ -1,15 +1,9 @@
 #!/usr/bin/env node
 var fs = require('fs')
 var path = require('path')
-
 var mkdirp = require('mkdirp')
-var argv = require('minimist')(process.argv.slice(2))
-
-var outfile = argv.o || argv.outfile
+var outfile = process.argv[2]
 
 mkdirp.sync(path.dirname(outfile))
-
-var ws = fs.createWriteStream(outfile)
-
-process.stdin.pipe(ws)
+process.stdin.pipe(fs.createWriteStream(outfile))
 
